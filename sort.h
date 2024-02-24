4bfc71e7d117d321995241048591cf13b690e6de
wilmaxgp <dennisanietimfonwilliam005@gmail.com>
2024-02-24 04:03:13 -0800

    wrote a function that sorts an array of integers in ascending order using the Selection sort algorithm

--- sort.h
+++ sort.h
@@ -21,5 +21,7 @@ void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void print_list(const listint_t *list);
 void insertion_sort_list(listint_t **list);
+void print_array(const int *array, size_t size);
+void selection_sort(int *array, size_t size);
 
 #endif
