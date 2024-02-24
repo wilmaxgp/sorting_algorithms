f7564aaa6e119cfe0f777db9b32bd3aa12b80e99
wilmaxgp <dennisanietimfonwilliam005@gmail.com>
2024-02-24 05:06:07 -0800

    Implemented Shell sort algorithm with Knuth sequence and added print statements for array during sorting

--- sort.h
+++ sort.h
@@ -25,5 +25,6 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void quicksort_helper(int *array, int low, int high, size_t size);
 int lomuto_partition(int *array, int low, int high, size_t size);
+void shell_sort(int *array, size_t size);
 
 #endif
